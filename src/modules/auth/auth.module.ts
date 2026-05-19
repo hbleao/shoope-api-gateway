adff7d512cbad30f352877fb559d01555f975abf
henrique leao <hbragaleao@gmail.com>
2026-05-19 17:54:59 -0300

    refactor: move folder to bounded contexts

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { HttpModule } from "@nestjs/axios";
+import { Module } from "@nestjs/common";
+import { ConfigModule, type ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { AuthService } from "./service/auth.service";
+
+@Module({
+	imports: [
+		PassportModule,
+		HttpModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				secret: configService.get("JWT_SECRET"),
+				signOptions: { expiresIn: "24h" },
+			}),
+			inject: [ConfigModule],
+		}),
+	],
+	providers: [AuthService],
+	exports: [],
+})
+export class AuthModule {}
